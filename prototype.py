c304434e4e24c5bc9fc00e8c32f850061aa0cf4e
Fred <speedup@gmail.com>
2009-09-01 02:29:49 +0200

    big heavy path finder algorithm that works

--- prototype.py
+++ prototype.py
@@ -22,6 +22,7 @@ class Station(object):
         self.id = id
         self.name = name
         self.lines = set()
+        self.schedules = set()
 
     def __unicode__(self):
         return u'station %s' % self.name
@@ -40,8 +41,14 @@ class Schedule(object):
         self.start_station.lines.add(self.line)
         self.end_station.lines.add(self.line)
 
+        self.start_station.schedules.add(self)
+        self.end_station.schedules.add(self)
+
     def __unicode__(self):
         return u'schedule from %s to %s on %s' % (self.start_station, self.end_station, self.line)
+
+    def __hash__(self):
+        return hash('%d%d%d' % (self.start_station.id, self.end_station.id, self.line.id))
 
 def get_lines_from_db(db_file):
     con = sqlite3.connect(db_file)
@@ -71,17 +78,98 @@ def get_lines_from_db(db_file):
 
     return lines, stations, schedules
 
+def neighbours(station, stations):
+    return [s for s in stations if s != station and [sc for sc in s.schedules if sc.start_station == station]]
+
+def find_path(start_station, end_station, lines, stations, schedules):
+    if not start_station or not end_station:
+        print 'Stations do not exits'
+        return []
+
+    print 'Looking for path from %s to %s' % (unicode(start_station), unicode(end_station))
+
+    # http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+    nodes = set(stations.values())
+
+    from sys import maxint
+    for n in nodes:
+        n.distance = maxint
+        n.previous = None
+
+    start_station.distance = 0
+
+    success = False
+    while len(nodes):
+        print '%d nodes left to scan' % len(nodes)
+
+        current = None
+        distance = maxint
+        for n in nodes:
+            print 'Analysing %s [%d] [%d]' % (unicode(n), n.distance, hash(n))
+            if n.distance < distance:
+                print 'New distance is %d for node %s' % (n.distance, unicode(n))
+                current = n
+                distance = n.distance
+
+        if current == None:
+            # Fail
+            print 'FAIL'
+            break
+
+        nodes2 = nodes.copy()
+        print 'Removing %s' % unicode(current)
+        nodes.remove(current)
+        print 'Intersect: %s' % ', '.join(unicode(n.name) for n in nodes2 ^ nodes)
+
+        print 'Handling %s' % unicode(current)
+        for s in neighbours(current, nodes):
+            distance = current.distance + 2
+            if distance < s.distance:
+                print 'Relaxing neighbour %s to %d' % (unicode(s), distance)
+                s.distance = distance
+                s.previous = current
+                if s == end_station:
+                    print 'Found the end station %s' % unicode(s)
+                    success = True
+                    break
+        if success:
+            break
+
+    path = []
+    s = end_station
+    while s != start_station:
+        if not s.previous:
+            break
+        path.append(s)
+        s = s.previous
+    path.append(start_station)
+    path.reverse()
+
+    return path
+
+def find_station(name, stations):
+    for s in stations.values():
+        if s.name == name:
+            return s
+
+    return None
 
 def main():
     lines, stations, schedules = get_lines_from_db(DB_FILE)
 
-    for l in lines.values():
-        print unicode(l)
+    # for l in lines.values():
+    #     print unicode(l)
 
-    for s in stations.values():
-        print unicode(s)
+    # for s in stations.values():
+    #     print '%d: %s' % (s.id, unicode(s.name))
 
-    for s in schedules:
+    # for s in schedules:
+    #     print unicode(s)
+
+    #print ', '.join([unicode(s.name) for s in neighbours(find_station('Commerce', stations), stations.values())])
+    #print '\n'.join([unicode(s) for s in find_station('Commerce', stations).schedules])
+
+    for s in find_path(find_station('Balard', stations), find_station('Oberkampf', stations), lines, stations, schedules):
         print unicode(s)
 
 if __name__ == "__main__":
